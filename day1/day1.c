6bb082e76ab91633b5c04163408d71a584b33ab5
wiede5335 <e1425647@student.tuwien.ac.at>
2018-12-05 22:51:55 +0100

    its faster now

--- day1/day1.c
+++ day1/day1.c
@@ -10,6 +10,7 @@ struct list{
 
 typedef struct list list;
 
+
 void insert(list *l, int n){
 	list *current = l;
 	list *insert;
@@ -49,10 +50,34 @@ int checkTwice(list *l, int sum){
 	return 0;
 }
 
+int checkTwiceArray(int **sort, int sum, int l, int o){
+
+	while(l<=o){
+		int m = (o+l)/2;
+
+		if((*sort)[m]<sum)
+			l = m+1;
+		else if((*sort)[m]>sum)
+			o=m-1;
+		else
+			return 1;	
+	}
+	return 0;
+}
+
+void fill(int *sort, list *l){
+	int i=0;
+	while(l!=NULL){
+		(sort)[i] = l->part;
+		l=l->next;
+		i++;
+	}
+}
+
 int main(int argc, char * args[]){
 	char buf[100];
 	int current;
-	int *nums = (int*)malloc(sizeof(int*));
+	int *nums = (int*)malloc(sizeof(int));
 	int c =1;
 	fgets(buf, sizeof buf, stdin);
 	list l; l.part = strtol(buf, NULL, 10); l.next=NULL;
@@ -64,20 +89,23 @@ int main(int argc, char * args[]){
 		sum+=current;
 		insert(&l, sum);
 		c++;
-		nums = realloc(nums, c*sizeof(int*));
+		nums = realloc(nums, c*sizeof(int));
 		nums[c-1] = current;
 	}
+	int *sort = (int*)malloc(c*sizeof(int));
+	fill(sort, &l);
 
 	printf("sum is: %d\n", sum);
 	for(int j =0; j<c; j++){
 		for(int i = 0; i <c; i++){
 			sum+=nums[i]; 
-			if(checkTwice(&l, sum)){
+			if(checkTwiceArray(&sort, sum, 0, c-1)){
 				printf("first double freq: %d\n", sum);
 				return 0;
 			}
 		}
 	}
+	printf("nothing found!\n");
 	free(nums);
 	return 0;
 }
